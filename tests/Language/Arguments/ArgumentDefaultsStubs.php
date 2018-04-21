a56037f1c065acab5b1e9c6d1db47c5512c6f9b1
Serafim <nesk@xakep.ru>
2018-04-21 05:03:06 +0300

    Fix tests

--- tests/Language/Arguments/ArgumentDefaultsStubs.php
+++ tests/Language/Arguments/ArgumentDefaultsStubs.php
@@ -137,4 +137,49 @@ trait ArgumentDefaultsStubs
             }
         }
     }
+
+    /**
+     * @param \Closure $execution
+     * @param string $suffix
+     * @return void
+     * @throws \LogicException
+     */
+    protected function positiveTestWrapper(\Closure $execution, string $suffix): void
+    {
+        try {
+            $execution();
+            $this->assertTrue(true);
+        } catch (\Throwable $e) {
+            throw new \LogicException(
+                (string)$e->getMessage() . "\n" .
+                'Should be successful:' . "\n" .
+                $suffix . "\n" .
+                \str_repeat('-', 60) . "\n"
+            );
+        }
+    }
+    /**
+     * @param \Closure $execution
+     * @param string $suffix
+     * @param string $error
+     * @return void
+     */
+    protected function negativeTestWrapper(\Closure $execution, string $suffix, string $error = \Throwable::class): void
+    {
+        try {
+            $execution();
+            $this->assertFalse(true,
+                'Should throw an error:' . "\n" .
+                $suffix . "\n" .
+                \str_repeat('-', 60) . "\n"
+            );
+        } catch (\Throwable $e) {
+            $this->assertInstanceOf($error, $e,
+                'Error must be an instance of ' . $error . ' but ' . \get_class($e) . ' given:' . "\n" .
+                (string)$e . "\n" .
+                $suffix . "\n" .
+                \str_repeat('-', 60) . "\n"
+            );
+        }
+    }
 }
