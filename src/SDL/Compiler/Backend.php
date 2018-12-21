1aa99a18df7f971017055240fb64c2e05809ba9b
Serafim <nesk@xakep.ru>
2018-12-21 14:58:43 +0300

    Merge branch 'master' of github.com:railt/railt

--- src/SDL/Compiler/Backend.php
+++ src/SDL/Compiler/Backend.php
@@ -24,43 +24,6 @@ use Railt\SDL\Exception\TypeException;
 class Backend
 {
     /**
-     * @var string
-     */
-    private const ERR_UNSUPPORTED_RULE = '%s is not supported by this compiler';
-
-    /**
-     * @var string
-     */
-    private const ERR_UNRECOGNIZED_RULE = 'The language rule <%s> is not supported or implemented by this compiler';
-
-    /**
-     * @var string[]
-     */
-    private const BUILDERS = [
-        // Root definitions
-        'SchemaDefinition'    => Definition\SchemaBuilder::class,
-        'ObjectDefinition'    => Definition\ObjectBuilder::class,
-        'EnumDefinition'      => Definition\EnumBuilder::class,
-        'ScalarDefinition'    => Definition\ScalarBuilder::class,
-        'InputDefinition'     => Definition\InputBuilder::class,
-        'DirectiveDefinition' => Definition\DirectiveBuilder::class,
-        'InterfaceDefinition' => Definition\InterfaceBuilder::class,
-        'UnionDefinition'     => Definition\UnionBuilder::class,
-        // Invocations
-        'Directive'           => Invocation\DirectiveBuilder::class,
-    ];
-
-    /**
-     * @var string[]
-     */
-    private const UNSUPPORTED_BUILDERS = [
-        'QueryOperation'        => 'Query expression',
-        'MutationOperation'     => 'Mutation expression',
-        'SubscriptionOperation' => 'Subscription expression',
-        'FragmentDefinition'    => 'Fragment definition',
-    ];
-
-    /**
      * @var Process
      */
     private $process;
@@ -72,76 +35,5 @@ class Backend
     public function __construct(Process $process)
     {
         $this->process = $process;
-    }
-
-    /**
-     * @param \Closure $deferred
-     */
-    public function async(\Closure $deferred): void
-    {
-        $this->process->async($deferred);
-    }
-
-    /**
-     * @param Document $document
-     * @return Context
-     */
-    public function context(Document $document): Context
-    {
-        return new Context($document->getFile(), $document);
-    }
-
-    /**
-     * @param Context $context
-     * @param RuleInterface $ast
-     * @return Document
-     * @throws TypeException
-     */
-    public function each(Context $context, RuleInterface $ast): Document
-    {
-        /** @var \Railt\Reflection\Document $document */
-        $document = $context->getDocument();
-
-        foreach ($ast->getChildren() as $child) {
-            $definition = $this->exec($context, $child);
-
-            $document->withDefinition($definition);
-        }
-
-        return $document;
-    }
-
-    /**
-     * @param Context $context
-     * @param RuleInterface $ast
-     * @return DefinitionInterface
-     * @throws TypeException
-     */
-    public function exec(Context $context, RuleInterface $ast): DefinitionInterface
-    {
-        if ($builder = self::BUILDERS[$ast->getName()] ?? null) {
-            return $this->execBuilder(new $builder($context, $ast, $this));
-        }
-
-        if ($unsupported = self::UNSUPPORTED_BUILDERS[$ast->getName()] ?? null) {
-            $exception = new TypeException(\sprintf(self::ERR_UNSUPPORTED_RULE, $unsupported));
-            $exception->throwsIn($context->getFile(), $ast->getOffset());
-
-            throw $exception;
-        }
-
-        $exception = new TypeException(\sprintf(self::ERR_UNRECOGNIZED_RULE, $ast->getName()));
-        $exception->throwsIn($context->getFile(), $ast->getOffset());
-
-        throw $exception;
-    }
-
-    /**
-     * @param BuilderInterface $builder
-     * @return DefinitionInterface
-     */
-    private function execBuilder(BuilderInterface $builder): DefinitionInterface
-    {
-        return $builder->build();
     }
 }
