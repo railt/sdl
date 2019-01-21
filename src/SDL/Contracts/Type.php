3a77445bffeb45993440965cc761cdf2486a0338
Kirill Nesmeyanov <nesk@xakep.ru>
2019-01-21 23:15:26 +0000

    Apply fixes from StyleCI

--- src/SDL/Contracts/Type.php
+++ src/SDL/Contracts/Type.php
@@ -25,31 +25,31 @@ interface Type
      */
     public const SCHEMA = 'Schema';
 
-    public const OBJECT = 'Object';
-    public const OBJECT_FIELD = 'Field';      // TODO Rename
+    public const OBJECT          = 'Object';
+    public const OBJECT_FIELD    = 'Field';      // TODO Rename
     public const OBJECT_ARGUMENT = 'Argument';   // TODO Rename
 
-    public const INTERFACE = 'Interface';
-    public const INTERFACE_FIELD = 'Field';      // TODO Rename
+    public const INTERFACE          = 'Interface';
+    public const INTERFACE_FIELD    = 'Field';      // TODO Rename
     public const INTERFACE_ARGUMENT = 'Argument';   // TODO Rename
 
-    public const DIRECTIVE = 'Directive';
+    public const DIRECTIVE          = 'Directive';
     public const DIRECTIVE_ARGUMENT = 'Argument';   // TODO Rename
 
-    public const INPUT = 'Input';
+    public const INPUT       = 'Input';
     public const INPUT_FIELD = 'InputField';
 
-    public const ENUM = 'Enum';
+    public const ENUM       = 'Enum';
     public const ENUM_VALUE = 'EnumValue';
 
-    public const UNION = 'Union';
+    public const UNION  = 'Union';
     public const SCALAR = 'Scalar';
     /**#@-*/
 
     /**#@+
      * Invocations
      */
-    public const INPUT_INVOCATION = self::INPUT;
+    public const INPUT_INVOCATION     = self::INPUT;
     public const DIRECTIVE_INVOCATION = self::DIRECTIVE;
     // Extensions
     public const EXTENSION = 'Extension';
